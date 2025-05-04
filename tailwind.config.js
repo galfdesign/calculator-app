c8aeafd4c5294d0c757f48e342f6bafbc5fa6dff
galfdesign <spb@galf.ru>
2025-05-04 14:20:04 +0300

    Fix Vercel 404 error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx}",
-    "./public/index.html"
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./src/index.html"
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [],
 }
